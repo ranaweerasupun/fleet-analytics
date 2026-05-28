f8ecc57dcfd6289b5e0f9c18aa912b2d4dc9f403
Supun Akalanka <supunsriyananda@gmail.com>
2026-05-28 14:30:39 +0530

    created: weights for the fleets sim

---
+++ fleet_simulator.py
@@ -0,0 +1,24 @@
+"""
+fleet_simulator.py
+"""
+
+import argparse
+import threading
+import random
+import time
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(__file__))
+from device_simulator import DeviceSimulator, DEVICE_PROFILES
+
+DEVICE_TYPES = list(DEVICE_PROFILES.keys())
+
+# Fixed distribution across types to mimic a real fleet
+TYPE_WEIGHTS = {
+    "sensor":     0.45,
+    "gateway":    0.20,
+    "camera":     0.20,
+    "controller": 0.15,
+}
+
